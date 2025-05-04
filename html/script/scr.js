0b364e31518d41434624a56a904cf97b1f977d58
tinkernegeroi <Roman.peskoff@gmail.com>
2025-05-04 20:21:54 +0300

    заебало

--- html/script/scr.js
+++ html/script/scr.js
@@ -7,38 +7,90 @@ var Answers = {
     question5: "сообщить в службу спасения",
     question6: ["a", "d"], // Проверка проводки и датчики дыма
     question7: "a", // Электропроводку
-    question8: ["101", "112"], // номер пожарной службы
+    question8: 'нет', // номер пожарной службы
     question9: "ежемесячно",
     question10: "b" // Ползком
 };
+var userAnswers = {
+    question1: 0,
+    question2: 0,
+    question3: 0,
+    question4: 0,
+    question5: 0,
+    question6: 0,
+    question7: 0,
+    question8: 0,
+    question9: 0,
+    question10: 0
+};
+
 
 function getFormData(e) {
     e.preventDefault();
-    const form = document.forms.test;
-    const formData = new FormData(form);
-
-    // Преобразуем FormData в объект
-    const data = {};
-    formData.forEach((value, key) => {
-        data[key] = value;
+    const FORM = document.forms.test;
+    const FORMDATA = new FormData(FORM);
+    const CHECKBOXES = document.querySelectorAll('input[name="question6"]:checked');
+    const QUESTION6VALUES = Array.from(CHECKBOXES).map(checkbox => checkbox.value);
+    const DATA = {};
+    FORMDATA.forEach((value, key) => {
+        DATA[key] = value;
     });
-    console.log(data);
-    createTable(data);
-    checkAnswers(data);
+    DATA.question6 = QUESTION6VALUES;
+    createTable(DATA);
+    checkAnswers(DATA);
 };
 function createTable(data) {
     const TABLE = document.querySelector("#results");
-    const ROW = document.createElement("tr");
-    ROW.innerHTML = ``;
+    const TABLEBODY = document.createElement("tbody");
+    const FIRSTROW = document.createElement("tr");
+    FIRSTROW.innerHTML = `<th>Номер вопроса</th>
+                <th>Вопрос</th>
+                <th>Ваш ответ</th>
+                <th>Правильный ответ</th>
+                <th>Балл</th>`;
+    TABLE.appendChild(TABLEBODY);
+    TABLEBODY.appendChild(FIRSTROW);
+    for (let i = 1; i <= 10; i++) {
+        const ROW = document.createElement("tr");
+        const TEXTOFQUESTION = document.querySelector(`#question-text${i}`).textContent;
+        const QUESTIONTEXT = TEXTOFQUESTION.replace(/^\D*(\d+)/, '');
+        if (data["question${i}"] in ['a', 'b', 'c', 'd']){
+            const ID1 = data["question${i}"].toLowerCase();
+            const TEXTOFANSWER = document.querySelector('#question${i}${ID1}"');
+            const ID2 = data['Answers["question${i}"]'];
+            const TEXTOFCORRECTANSWER = document.querySelector('#question${i}${ID2}');
+        }
+        else{
+            const TEXTOFANSWER = data["question${i}"];
+            const TEXTOFCORRECTANSWER = Answers["question${i}"];
+        }
+
+        console.log(TEXTOFANSWER);
+        console.log(TEXTOFCORRECTANSWER);
+        ROW.innerHTML = `
+        <td>${i}</td>
+        <td>${QUESTIONTEXT}</td>
+        <td>${TEXTOFANSWER}</td>
+        <td>${TEXTOFCORRECTANSWER}</td>`;
+        TABLEBODY.appendChild(ROW);
+
+    }
 }
 function checkAnswers(data) {
-    var userAnswers = {};
-    for (const question in Answers) {
-        if (data[question]){
-            const userAnswer = data[question];
-            const correctAnswer = Answers[question];
-            if (userAnswer.toString().toLowerCase() === correctAnswer.toString().toLowerCase()){
-                userAnswers[question] = 1;
+    for (const QUESTIONS in Answers) {
+        if(QUESTIONS == "question6"){
+            const USERANSWER = data[QUESTIONS];
+            const CORRECTANSWER = Answers[QUESTIONS];
+            if (USERANSWER === CORRECTANSWER){
+                userAnswers[QUESTIONS] = 1;
+            }
+        }
+
+        else if (data[QUESTIONS]){
+            const USERANSWER = data[QUESTIONS];
+            const CORRECTANSWER = Answers[QUESTIONS];
+            if (USERANSWER.toLowerCase() == CORRECTANSWER.toLowerCase()){
+                userAnswers[QUESTIONS] = 1;
             }
         }
     }
